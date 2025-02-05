ea41652fa9143195b73f4cd957437c86b42a8de3
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-05 11:47:35 +0330

    [ADD] typescript support

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,23 +4,23 @@ import { initReactI18next } from "react-i18next";
 import enTranslation from "./locales/en/translation.json";
 import faTranslation from "./locales/fa/translation.json";
 
-console.log(enTranslation);
+export const defaultNS = "translation";
+
+export const resources = {
+  en: {
+    translation: enTranslation,
+  },
+  fa: {
+    translation: faTranslation,
+  },
+} as const;
 
 i18n.use(initReactI18next).init({
-  // the translations
-  // (tip move them in a JSON file and import them,
-  // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-  resources: {
-    en: {
-      translation: enTranslation,
-    },
-    fa: {
-      translation: faTranslation,
-    },
-  },
+  resources,
+  ns: ["translation"],
+  defaultNS,
   lng: "fa",
   fallbackLng: "en",
-
   interpolation: {
     escapeValue: false,
   },
