4eb73ce8b875227c4d841b0ab56a44eba2f1e0d2
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-05 11:38:36 +0330

    [ADD] update i18n's language based on provider's

--- src/providers/DictionaryProvider.tsx
+++ src/providers/DictionaryProvider.tsx
@@ -7,6 +7,8 @@ import {
   useState,
 } from "react";
 
+import i18n from "i18next";
+
 import { LANGUAGE_KEY } from "@/constants/local-storage.constants.ts";
 
 const defaultLanguage = (): string => {
@@ -30,6 +32,8 @@ function DictionaryProvider({ children }: PropsWithChildren) {
   const [language, setLanguage] = useState<string>(defaultLanguage);
 
   useEffect(() => {
+    i18n.changeLanguage(language);
+
     localStorage.setItem(LANGUAGE_KEY, language);
     document.documentElement.dir = language === "fa" ? "rtl" : "ltr";
   }, [language]);
