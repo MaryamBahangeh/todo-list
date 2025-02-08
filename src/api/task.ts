4bc1906508d4759e904e4eeb65ea14841826e5f5
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-08 10:50:06 +0330

    [ADD] base url constant

--- src/api/task.ts
+++ src/api/task.ts
@@ -1,32 +1,34 @@
 import { Task } from "@/models/task.ts";
 
+const BASE_URL = "http://localhost:3000/tasks";
+
 export const fetchTasks = async (): Promise<Task[]> => {
-  const response = await fetch("http://localhost:3000/tasks");
+  const response = await fetch(`${BASE_URL}`);
   return await response.json();
 };
 
 export const addTaskApi = async (task: Task): Promise<void> => {
-  await fetch(`http://localhost:3000/tasks`, {
+  await fetch(`${BASE_URL}`, {
     method: "POST",
     body: JSON.stringify(task),
   });
 };
 
 export const updateTaskApi = async (task: Task): Promise<void> => {
-  await fetch(`http://localhost:3000/tasks/${task.id}`, {
+  await fetch(`${BASE_URL}/${task.id}`, {
     method: "PUT",
     body: JSON.stringify(task),
   });
 };
 
 export const deleteTaskApi = async (id: string): Promise<void> => {
-  await fetch(`http://localhost:3000/tasks/${id}`, {
+  await fetch(`${BASE_URL}/${id}`, {
     method: "DELETE",
   });
 };
 
 export const filterTasksApi = async (name: string): Promise<Task[]> => {
-  const response = await fetch(`http://localhost:3000/tasks?name=${name}`, {
+  const response = await fetch(`${BASE_URL}?name=${name}`, {
     method: "GET",
   });
   return await response.json();
